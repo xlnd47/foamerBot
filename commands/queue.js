619a14dd3997c39449b9ce5f3edbb0e689447590
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-16 16:32:33 +0100

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args, con) => {
     //this is where the actual code for the command goes
-    let sql = `select * from playlist where guildId = "${message.guild.id}" and played = 0`;
+    let sql = `select * from playlist where guildId = "${message.guild.id}" and played = 0  order by dateAdded `;
     console.log(sql);
     con.query(sql, (err, rows) => {
         console.log(err);
