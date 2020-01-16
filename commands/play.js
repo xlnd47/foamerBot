5f1ed3823885cba50594ad2bf5457173c21a6038
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-16 15:04:40 +0100

    Create play.js

---
+++ commands/play.js
@@ -0,0 +1,22 @@
+const ytdl = require('ytdl-core-discord');
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args, con) => {
+    //this is where the actual code for the command goes
+
+    // if (args[0].length < 1)
+    //     return message.reply(`gimme link bru, prefer youtube link`);
+
+    console.log(message.member.voiceChannel);
+
+}
+//name this whatever the command name is.
+module.exports.help = {
+  name: "example",
+  description: "Example"
+
+}
+
+async function play(connection, url) {
+    connection.play(await ytdl(url), { type: 'opus' });
+}
