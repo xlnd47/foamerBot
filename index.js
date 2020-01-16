db6ec9d904997f678e65c5bb694dad87b0e7eb2f
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-16 13:58:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ bot.on("message", async message => {
 
   con.query(`select * from xp where discordId = "${message.author.id}"`, (err, rows) => {
     if (err) throw err;
-
+    //console.log(rows);
 
     let sql1;
     if(rows.length < 1){
@@ -90,7 +90,7 @@ bot.on("message", async message => {
       sql1 = `update xp set xp = ${xp + generateXp()} where discordId = "${message.author.id}"`;
     }
 
-    con.query(sql, console.log);
+    con.query(sql1, console.log);
 
   })
 
