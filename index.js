ab4cd7776e2786e5ba957aa39de1bc766e024af9
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-16 20:23:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ function checkLevelUp(message){
 }
 
 function levelUp(id, message, lvl){
-  let sql3 = `update level set level = ${lvl + 1} where discordId = "${id}"`; 
+  let sql3 = `update xp set level = ${lvl + 1} where discordId = "${id}"`; 
   con.query(sql3);
 
   message.reply(`bru, ge zijt level ${lvl + 1} nu`);
