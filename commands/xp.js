a0fa0ac527a25233c204ebb6b68fae4a55e88c0b
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-16 14:15:16 +0100

    Create xp.js

---
+++ commands/xp.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args, con) => {
+  //this is where the actual code for the command goes
+  let sql = `select * from xp where discordId = "${message.author.id}"`;
+
+  con.query(sql, (err, rows) => { 
+    message.reply(`jij heb ${rows[0].xp} xp, bru`);
+  }
+
+
+}
+//name this whatever the command name is.
+module.exports.help = {
+  name: "xp",
+  description: "xp"
+
+}
