7667663ab6991b4caef826a9fc8969cbe8b86610
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-16 18:31:48 +0100

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -1,12 +1,26 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
 
 module.exports.run = async (bot, message, args, con) => {
     //this is where the actual code for the command goes
     let sql = `select * from playlist where guildId = "${message.guild.id}" and played = 0  order by dateAdded `;
     console.log(sql);
     con.query(sql, (err, rows) => {
-        console.log(err);
+        //console.log(err);
         console.log(rows);
+
+        rows.forEach(e => {
+            ytdl.getInfo(e.urlId, (err, info) => {
+                if (err) throw err;
+                let format = ytdl.chooseFormat(info.formats, { quality: '134' });
+                if (format) {
+                  console.log('Format found!');
+                }
+
+                console.log(info);
+              });
+        });
+
     })
 }
 //name this whatever the command name is.
