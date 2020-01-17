b478b04ed168bf5935ad979d682fabf566ed7235
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-17 23:11:58 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,13 +1,30 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+const mysql = require('mysql');
+const ytdl = require(`ytdl-core`);
+var con;
 
-module.exports.run = async (bot, message, args, con) => {
-    //this is where the actual code for the command goes
-
-    // if (args[0].length < 1)
-    //     return message.reply(`gimme link bru, prefer youtube link`);
+module.exports.run = async (bot, message, args, conn) => {
 
     console.log(message.member);
     // console.log(message.user);
+    console.log(message.author.client.voiceConnections);
+    con = con; 
+    // try {
+    //   let sql = `select * from playlist where guildId = "${message.guild.id} and played = 0"`;
+    //   con.query(sql, (err, result) => {
+    //     if(result[0].length < 1)
+    //       return message.reply(`niks in queue, bruh`);
+
+    //     let firstSong = result[0];
+    //     voice_con.playStream(ytdl(`https://www.youtube.com/watch?v=${firstSong.urlId}`, {
+    //       filter: `audioonly`,
+    //       quality: `highestaudio`
+    //     }))
+
+    //   });
+    // }catch(e){
+
+    // }
 
 
 }
