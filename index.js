95fee6d4f747f2dd3e47960ec98be3a96943551c
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-17 14:12:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -157,11 +157,14 @@ function checkLevelUp(message){
     }
   });
 
+
 }
 
 function levelUp(id, message, lvl){
   let sql3 = `update xp set level = ${lvl + 1} where discordId = "${id}"`; 
   con.query(sql3);
+
+  message.reply(`bruh, ge zijt ${lvl + 1} nu bruh`);
 
 }
 
