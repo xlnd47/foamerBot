419be14c10248e3b3b0322116344b03a12a1bf4b
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-18 20:19:00 +0100

    Update level.js

--- commands/level.js
+++ commands/level.js
@@ -3,15 +3,19 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args, con) => {
     //this is where the actual code for the command goes
     let sql = `select * from xp where discordId = "${message.author.id}"`;
-
     con.query(sql, (err, rows) => { 
         if (rows[0] == undefined)
             return  message.reply(`jij heb geen xp, bruh`);
-        message.reply(`jij ben lvl ${rows[0].level} en heb ${rows[0].xp} xp, bruh`);
+        
+        let xpNeeded = 5 * (rows[0].level ^ 2) + 50 * rows[0].level + 100;
+        message.reply(`jij ben lvl ${rows[0].level}, heb ${rows[0].xp} xp en ${xpNeeded} nodig voor te levelen, bruh`);
     })
 
 
 }
+
+
+
 //name this whatever the command name is.
 module.exports.help = {
   name: "level",
