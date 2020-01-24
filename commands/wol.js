9ecfc2af16e5a2955650d9a0a1db816dc72f4964
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-24 13:39:09 +0100

    Update wol.js

--- commands/wol.js
+++ commands/wol.js
@@ -13,11 +13,11 @@ module.exports.run = async (bot, message, args, con) => {
 
     var name = args[0];
     request({
-        uri: `https://wol.gg/stats/euw/${name}`,
+        uri: `https://wol.gg/stats/euw/${name}/`,
       }, function(error, response, body) {
         var $ = cheerio.load(body.replace(/<!--|-->/g, ''))
-        message.reply(`You wasted ${$('#time-days').text()} playing lol...`)
-        //console.log($('#time-days').text());
+        //message.reply(`You wasted ${$('#time-days').text()} playing lol...`)
+        console.log($('#time-days').text());
       });
 
 
