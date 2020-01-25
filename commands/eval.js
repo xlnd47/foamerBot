c05445fb37fadb8e503e620f7ded739f99c441c0
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-25 18:01:28 +0100

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -32,12 +32,11 @@ module.exports.run = async (bot, message, args, con) => {
 
 function clean(text) {
     if (typeof(text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+        return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
     else
         return text;
-  }
+    }
 module.exports.help = {
   name: "eval",
   description: "Evaluation command, only for developers"
-
 }
