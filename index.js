16bc482f8033ae32841bb8648322aaa74f4b0e78
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-27 21:02:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -69,8 +69,11 @@ fs.readdir("./commands/", (err, files) => {
 
 jsfile.forEach((f, i) =>{
   let props = require(`./commands/${f}`);
-  console.log(`${f} loaded!`);
-  bot.commands.set(props.help.name, props);
+  //console.log(`${f} loaded!`);
+  props.help.name.forEach((n, i) => {
+    console.log(`${f}, ${n} loaded!`);
+    bot.commands.set(n, props);
+  })
 });
 
 });
