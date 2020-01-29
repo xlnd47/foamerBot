78ca28f69602a2c78166d55b3cb8cce04aa141d9
xlnd47 <axel.deschuijmer@gmail.com>
2020-01-29 14:51:19 +0100

    Update level.js

--- commands/level.js
+++ commands/level.js
@@ -1,18 +1,6 @@
 const Discord = require('discord.js');
 
-
-
-
 module.exports.run = async (bot, message, args, con) => {
-    //this is where the actual code for the command goes
-    // let sql = `select * from xp where discordId = "${message.author.id}"`;
-    // con.query(sql, (err, rows) => { 
-    //     if (rows[0] == undefined)
-    //         return  message.reply(`jij heb geen xp, bruh`);
-        
-    //     let xpNeeded = 5 * (rows[0].level ^ 2) + 50 * rows[0].level + 100;
-    //     message.reply(`jij ben lvl ${rows[0].level}, heb ${rows[0].xp}/${xpNeeded} xp, bruh`);
-    // })
 
     let sql = `select x.discordId, x.level, x.xp, r.honor, r.report From foamer.xp x left join foamer.reputation r on x.discordId = r.discordId  where x.discordId = "${message.author.id}"`;
     con.query(sql, (err, result) => { 
@@ -41,7 +29,7 @@ module.exports.run = async (bot, message, args, con) => {
 
 //name this whatever the command name is.
 module.exports.help = {
-  name: "level",
+  name: ["level", "stats", "lvl"],
   description: "level"
 
 }
